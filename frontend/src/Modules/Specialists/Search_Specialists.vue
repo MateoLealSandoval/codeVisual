ba42c04d6b078f8e6c426c93b656a4a7c91131d4
Mateo Leal Sandoval <mlealsandoval@gmail.com>
2025-08-07 21:21:05 -0500

    .

--- frontend/src/Modules/Specialists/Search_Specialists.vue
+++ frontend/src/Modules/Specialists/Search_Specialists.vue
@@ -171,10 +171,9 @@ export default {
             state.filterService = [];
         };
 
-        // Función para manejar Enter
+        <!-- Función para manejar Enter -->
         function handleEnter(event: KeyboardEvent) {
             if (event.key === 'Enter') {
-                togglePanel('panelService');
                 load_data();
             }
         }
