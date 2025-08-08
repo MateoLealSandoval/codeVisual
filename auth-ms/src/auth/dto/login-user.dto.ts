5c7bc777a4a50ef04ccc943bc0a4d4d4f8b461cc
Mateo Leal Sandoval <mlealsandoval@gmail.com>
2025-08-07 22:21:26 -0500

    .

--- auth-ms/src/auth/dto/login-user.dto.ts
+++ auth-ms/src/auth/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
-import { IsEmail, IsString, IsStrongPassword } from "class-validator";
+import { IsEmail, IsString, Matches } from "class-validator";
 
-export class LoginUserDto{
+export class LoginUserDto {
     @IsString()
     @IsEmail()
-    email:string;
+    email: string;
+    
     @IsString()
-    @IsStrongPassword()
-    password:string;
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z]).{8,}$/, {
+        message: 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula y una letra minúscula',
+    })
+    password: string;
 }
